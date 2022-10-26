a7e3ffaa1c2dee5329fa5ec8b9a406aa557f8a4e
Ilena Acioli dos Santos <ilenaaciolids@gmail.com>
2022-10-26 15:13:06 -0300

    modificando base_url

--- frontend/src/Constants/Urls.js
+++ frontend/src/Constants/Urls.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://ec2-174-129-180-229.compute-1.amazonaws.com"
-export const BASE_URL = "http://localhost:3003"
+export const BASE_URL = "http://ec2-174-129-180-229.compute-1.amazonaws.com"
+// export const BASE_URL = "http://localhost:3003"
